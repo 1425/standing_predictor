3d1073819e564ba81599b9ae2d83b2d8c7b1560c
Eric Rippey <rippey.e@gmail.com>
2026-03-04 00:21:55 -0800

    Remove unused features

--- outline.cpp
+++ outline.cpp
@@ -73,7 +73,6 @@ std::tuple<Run_result,Points_used,By_team> run_inner(
 	bool ignore_chairmans,
 	tba::District_key district,
 	tba::Year year,
-	std::vector<int> dcmp_size,
 	Skill_method skill_method,
 	bool quick
 ){
@@ -185,43 +184,6 @@ std::tuple<Run_result,Points_used,By_team> run_inner(
 		by_team[team.team_key]=Team_status(chairmans.count(team.team_key),std::move(dist),dcmp_home,team.point_total);
 	}
 
-	map<Point,Pr> by_points; //# of teams expected to end at each # of points
-	for(auto [team,data1]:by_team){
-		auto [cm,data,dcmp_home,point_total]=data1;
-		(void)team;
-		for(auto [pts,pr]:data){
-			auto f=by_points.find(pts);
-			if(f==by_points.end()){
-				by_points[pts]=pr;
-			}else{
-				f->second+=pr;
-			}
-		}
-	}
-
-	bool sum_display=0;
-	if(sum_display){
-		for(auto [pts,pr]:by_points){
-			cout<<pts<<","<<pr<<"\n";
-		}
-	}
-
-	bool cdf_display=0;
-	if(cdf_display){
-		map<Point,Pr> cdf;
-		{
-			double d=0;
-			for(auto [pts,pr]:by_points){
-				d+=pr;
-				cdf[pts]=d;
-			}
-		}
-
-		for(auto [pts,pr]:cdf){
-			cout<<pts<<","<<pr<<"\n";
-		}
-	}
-
 	Run_input run_input=read_status(f,district,skill_method);
 	run_input.quick=quick;
 
@@ -230,25 +192,6 @@ std::tuple<Run_result,Points_used,By_team> run_inner(
 		points_used,
 		by_team
 	);
-	/*return make_tuple(
-		run_calc(Run_input{
-			worlds_slots(district),
-			by_team,
-			[=](){
-				vector<Dcmp_data> r;
-				for(auto [i,x]:enumerate(dcmp_size)){
-					Dcmp_data d;
-					d.size=x;
-					d.played=dcmp_played;
-					d.dists=skills.at_dcmp;
-					r|=d;
-				}
-				return r;
-			}()
-		}),
-		points_used,
-		by_team
-	);*/
 }
 
 struct Run_inputs{
@@ -265,81 +208,14 @@ struct Run_inputs{
 	bool quick=0;
 };
 
-auto plot(flat_map2<short int,double> a,auto title){
-	using P=std::pair<int,double>;
-	std::vector<P> data;
-	for(auto [k,v]:a){
-		data|=P(k,v);
-	}
-	return plot(data,::as_string(title));
-}
-
-void team_dists(District_key district,std::map<Team_key,Team_status> const& by_team){
-	std::vector<Plot_setup> setups;
-	for(auto [k,v]:by_team){
-		Plot_setup p;
-		p.title=::as_string(k);
-		p.data=mapf([](auto x){ return Plot_point2(x.first,x.second); },v.point_dist);
-		setups|=p;
-	}
-	auto plots=plot(setups);
-
-	ofstream f("teams_"+::as_string(district)+".html");
-	f<<"<html><body>\n";
-	for(auto [team_info,plot]:zip(by_team,plots)){
-		auto [k,v]=team_info;
-		f<<h2(k);
-		f<<quartiles(v.point_dist);
-		PRINT(k);
-		//PRINT(v);
-		//f<<plot(v.point_dist,k);
-		f<<plot;
-	}
-	f<<"</body></html>";
-}
-
 map<tba::Team_key,Pr> run(
 	TBA_fetcher& f,
 	Run_inputs inputs
 ){
 	//this function exists to separate the input & calculation from the output
-	auto district=*inputs.district;
-	auto year=*inputs.year;
-	auto [results,points_used,by_team]=run_inner(f,inputs.ignore_chairmans,district,year,inputs.dcmp_size,inputs.skill_method,inputs.quick);
-
-	//print_lines(by_team);
-	bool by_team_csv=0;
-	if(by_team_csv){
-		cout<<"team,";
-		for(auto i:range_st<140>()){
-			cout<<i<<",";
-		}
-		for(auto [team,data1]:by_team){
-			auto [cmd,data,dcmp_home,already_earned]=data1;
-			cout<<team<<",";
-			for(auto i:range_st<140>()){
-				auto f=data.find(i);
-				if(f==data.end()){
-					cout<<"0,";
-				}else{
-					cout<<f->second<<",";
-				}
-			}
-			cout<<"\n";
-		}
-	}
-
-	bool by_team_chart=0;
-	if(by_team_chart){
-		team_dists(district,by_team);
-	}
-
-	if(0){
-		cout<<"Championship cutoff\n";
-		for(auto x:results.cmp_cutoff_pr){
-			cout<<"\t"<<x<<"\n";
-		}
-	}
+	const auto district=*inputs.district;
+	const auto year=*inputs.year;
+	auto [results,points_used,by_team]=run_inner(f,inputs.ignore_chairmans,district,year,inputs.skill_method,inputs.quick);
 
 	auto team_info=district_teams(f,district);
 	{
@@ -360,25 +236,6 @@ map<tba::Team_key,Pr> run(
 		gen_html(file,ghi,annotated(f,district));
 	}
 
-	bool show_table=0;
-	if(show_table){
-		cout<<"Team #\tP(DCMP)\tPts 5%\tPts 50%\tPts 95%\tNickname\n";
-		cout.precision(3);
-		for(auto a:reversed(sorted(
-			results.result,
-			[](auto x){ return make_pair(x.dcmp_make,x); }
-		))){
-			cout<<a.team.str().substr(3,100)<<"\t";
-			cout<<a.dcmp_make<<"\t";
-			cout<<a.dcmp_interesting[0]<<"\t";
-			cout<<a.dcmp_interesting[1]<<"\t";
-			cout<<a.dcmp_interesting[2]<<"\t";
-			auto f=filter_unique([=](auto const& f){ return f.key==a.team; },team_info);
-			using namespace tba;
-			cout<<f.nickname<<"\n";
-		}
-	}
-
 	return to_map(::mapf(
 		[](auto x){
 			return make_pair(x.team,x.dcmp_make);
@@ -387,454 +244,12 @@ map<tba::Team_key,Pr> run(
 	));
 }
 
-#define TEAM_DATA_ITEMS(X)\
-	X(vector<double>,district_points_earned)\
-	X(int,unplayed_district_events_scheduled)\
-	X(bool,won_district_chairmans)\
-	X(optional<int>,dcmp_points)\
-	X(Dcmp_home,dcmp_home)
-
-STRUCT_DECLARE(Team_data,TEAM_DATA_ITEMS)
-PRINT_STRUCT(Team_data,TEAM_DATA_ITEMS)
-
-using District_data=map<tba::Team_key,Team_data>;
-
-Run_input to_run_input_equal(TBA_fetcher &fetcher,tba::District_key district,District_data data){
-	Run_input r;
-	//r.dcmp_size=dcmp_size(district);
-	r.worlds_slots=worlds_slots(district);
-
-	//this is the baseline for how many points at an upcoming district event
-	const auto pr=flat_map2(historical_event_pts(fetcher));
-
-	r.by_team=to_map(mapf(
-		[&](auto data)->std::pair<tba::Team_key,Team_status>{
-			auto [team,x]=data;
-			Dcmp_home dcmp_home=calc_dcmp_home(fetcher,team);
-			return make_pair(team,Team_status(
-				x.won_district_chairmans,
-				[=]()->Team_dist{
-					if(x.unplayed_district_events_scheduled==2){
-						return convolve(pr,pr);
-					}else if(x.unplayed_district_events_scheduled==1){
-						return pr+sum(x.district_points_earned);
-					}else if(x.unplayed_district_events_scheduled==0){
-						return Team_dist{{sum(x.district_points_earned),1}};
-					}else{
-						assert(0);
-					}
-				}(),
-				dcmp_home,
-				sum(x.district_points_earned)
-			));
-		},
-		data
-	));
-
-	/*r.dcmp_played=[&]()->bool{
-		auto e=tba::district_events_simple(fetcher,district);
-		auto m=to_set(mapf([](auto e){ return e.event_type; },e));
-		return m.count(tba::Event_type::DISTRICT_CMP);
-	}();*/
-	//r.dcmp_distribution1=;*/
-
-	auto d=tba::district_rankings(fetcher,district);
-	assert(d);
-
-	/*r.d1=mapf(
-		[=](auto x){
-			//PRINT(x);
-			x.event_points=filter([=](auto y){ return events.count(y.event_key); },x.event_points);
-			x.point_total=sum(mapf([](auto x){ return x.total; },x.event_points));
-			return x;
-		},
-		*d
-	);*/
-	//r.points_used=;*/
-
-	//vector<tba::District_ranking>
-
-	#if 0
-	r.d1=mapf(
-		[](auto x)->tba::District_Ranking{
-			auto [team,team_data]=x;
-			return tba::District_Ranking{
-				team_key,
-				rank,
-				rookie_bonus pts,
-				point total
-				vector<Event_points> event points; Event_points=
-			};
-		},
-		data
-	);
-
-	cout<<"\n\n";
-	print_lines(take(5,*d));
-	PRINT(data);
-	//points_used:=std::map<tba::Team_key,std::tuple<std::vector<int>,int,int>>
-	nyi
-	#endif
-	return r;
-}
-
-Run_input to_run_input_historical(
-	TBA_fetcher &fetcher,
-	tba::District_key district,
-	District_data data
-){
-	//allow the other version to put in most of the data
-	auto r=to_run_input_equal(fetcher,district,data);
-	//now, just put in alternate shapes for the distributions.
-	nyi
-	return r;
-}
-
-District_data partial_data(
-	TBA_fetcher &fetcher,
-	tba::District_key district,
-	std::set<tba::Event_key> const& events
-){
-	auto d=tba::district_rankings(fetcher,district);
-	assert(d);
-	District_data r;
-	for(auto team_data:*d){
-		auto team=team_data.team_key;
-		auto team_events=filter(
-			[&](auto x){ return events.count(x.event_key); },
-			team_data.event_points
-		);
-		auto g=group([](auto x){ return x.district_cmp; },team_events);
-		auto district_events=g[0];
-
-		//if third events show up here, will have to put in logic to look at the order the events happen in
-		assert(district_events.size()<=2);
-
-		Team_data td;
-		td.dcmp_home=calc_dcmp_home(fetcher,team);
-		td.district_points_earned=mapf([](auto x){ return x.total; },district_events);
-
-		//obviously not always true; FIXME
-		td.unplayed_district_events_scheduled=2-district_events.size();
-
-		auto m=mapf([&](auto event){ return tba::team_event_awards(fetcher,team,event); },events);
-		auto c=filter(
-			[](auto x)->bool{
-				return x.award_type==tba::Award_type::CHAIRMANS;
-			},
-			flatten(m)
-		);
-		td.won_district_chairmans=!c.empty();
-
-		auto cmp_events=g[1];
-		//this if is here so that can differentiate between 0 points earned and did not play.
-		if(!cmp_events.empty()){
-			td.dcmp_points=sum(mapf([](auto x){ return x.total; },cmp_events));
-		}
-		r[team]=td;
-	}
-	return r;
-}
-
-#if 0
-Run_input partial_data2(
-	TBA_fetcher &fetcher,
-	tba::District_key district,
-	std::set<tba::Event_key> const& events
-){
-	(void)events;
-	Run_input r;
-	r.dcmp_size=dcmp_size(district);
-	r.worlds_slots=worlds_slots(district);
-
-
-	//by_team=
-	/*r.by_team=[](){
-		//team to (chairmans,distribution by dcmp)
-		nyi
-	}();*/
-	r.dcmp_played=[&]()->bool{
-		auto e=district_events_simple(fetcher,district);
-		auto m=to_set(mapf([](auto e){ return e.event_type; },e));
-		return m.count(tba::Event_type::DISTRICT_CMP);
-	}();
-	//r.dcmp_distribution1=;*/
-
-	auto d=district_rankings(fetcher,district);
-	assert(d);
-
-	r.d1=mapf(
-		[=](auto x){
-			//PRINT(x);
-			x.event_points=filter([=](auto y){ return events.count(y.event_key); },x.event_points);
-			x.point_total=sum(mapf([](auto x){ return x.total; },x.event_points));
-			return x;
-		},
-		*d
-	);
-	//r.points_used=;*/
-	nyi
-	return r;
-}
-#endif
-
-vector<District_data> partial_data(TBA_fetcher &fetcher,tba::District_key district){
-	(void)fetcher;
-
-	auto events=district_events_simple(fetcher,district);
-	auto ends=to_set(mapf([](auto x){ return x.end_date; },events));
-	PRINT(ends);
-
-	vector<District_data> r;
-	for(auto date:ends){
-		auto f=filter([=](auto x){ return x.end_date<=date; },events);
-		//PRINT(date);
-		auto k=to_set(keys(f));
-		//PRINT(k);
-		r|=partial_data(fetcher,district,k);
-		//print_lines(r);
-	}
-	return r;
-}
-
-void analyze_status(std::map<Team,Team_data> const& a){
-	cout<<"Teams:"<<a.size()<<"\n";
-	#define X(A,B) auto B=mapf([](auto x){ return x.second.B; },a);
-	TEAM_DATA_ITEMS(X)
-	#undef X
-	auto m=mapf([](auto x){ return x.size(); },district_points_earned);
-	cout<<"Points eaned size:"<<count(m)<<"\n";
-	cout<<"Unplaiyed:"<<count(unplayed_district_events_scheduled)<<"\n";
-	cout<<"dcmp_points:"<<nonempty(dcmp_points).size()<<"\n";
-
-	{
-		auto d=to_set(dcmp_home);
-		if(d.size()>1){
-			cout<<"dcmp_home:"<<d<<"\n";
-		}
-	}
-	/*
-	district_points_earned
-	unplayed_district_events_scheduled
-	dcmp_points
-	dcmp_home*/
-}
-
-#define PREDICTION_STATUS(X)\
-	X(Team,team)\
-	X(int,district_events_remaining)\
-	X(Pr,dcmp_pr)\
-	X(Pr,cmp_pr)
-
-STRUCT_DECLARE(Prediction_status,PREDICTION_STATUS)
-PRINT_STRUCT(Prediction_status,PREDICTION_STATUS)
-
-std::vector<Prediction_status> historical_demo(TBA_fetcher &fetcher,tba::District_key const& district){
-	auto p=partial_data(fetcher,district);
-	//Run_input to_run_input_equal(TBA_fetcher &fetcher,tba::District_key district,District_data data){
-	mapf(
-		[&](auto data){
-			//print_lines(count(sorted(values(data))));
-			return to_run_input_equal(fetcher,district,data);
-		},
-		p
-	);
-
-	if(0){
-		map<Team,vector<Team_data>> m;
-		for(auto set_of_data:p){
-			for(auto [k,v]:set_of_data){
-				m[k]|=v;
-			}
-		}
-		print_r(m);
-		//nyi
-		
-	}
-
-	auto skill=skill_estimates(fetcher,district,Skill_method::NONE);
-	//auto skill=skill_estimates(fetcher,district,Skill_method::POINTS);
-	//auto skill=skill_estimates(fetcher,district,Skill_method::OPR);
-
-	//print_r(skill);
-	//nyi
-
-	vector<Prediction_status> r;
-
-	for(auto p1:p){
-		//cout<<"---------------------\n";
-		//analyze_status(p1);
-		Run_input input;
-		//input.dcmp_size=dcmp_size(district);
-		input.worlds_slots=worlds_slots(district);
-		input.by_team=to_map(mapf(
-			[&](auto p){
-				auto [team,data]=p;
-
-				Team_status r;
-				r.district_chairmans=data.won_district_chairmans;
-
-				if(data.unplayed_district_events_scheduled==2){
-					r.point_dist=skill.pre_dcmp[team];
-				}else if(data.unplayed_district_events_scheduled==1){
-					auto already=(Point)sum(data.district_points_earned);
-					auto next=skill.second_event[already];
-					r.point_dist=next+already;
-				}else if(data.unplayed_district_events_scheduled==0){
-					auto already=(Point)sum(data.district_points_earned);
-					r.point_dist[already]=1;
-				}else{
-					assert(0);
-				}
-				r.dcmp_home=calc_dcmp_home(fetcher,team);
-				r.already_earned=sum(data.district_points_earned);
-
-				return make_pair(team,r);
-			},
-			p1
-		));
-
-		//input.dcmp_played=any(mapf([](auto x){ return x.dcmp_points; },values(p1)));
-
-		//input.dcmp_distribution1=duplicate(skill.at_dcmp,MAX_DCMPS);
-
-		//print_r(input);
-		if(0){
-			auto i2=input;
-			auto bt=i2.by_team;
-			i2.by_team.clear();
-			//i2.dcmp_distribution1.clear();
-			print_r(i2);
-			for(auto [k,v]:take<5>(bt)){
-				//p.second.point_dist.clear();//because big onscreen.
-				//cout<<"\t"<<v<<"\n";
-
-				auto opts=keys(v.point_dist);
-				//cout<<"\t"<<k<<"\t"<<v.already_earned<<"\t"<<min(opts)<<"\t"<<max(opts)<<"\n";
-				auto t=make_tuple(v.already_earned,min(opts),max(opts));
-				auto t2=sorted(t);
-				if(t!=t2){
-					PRINT(t);
-				}
-				assert(t==t2);
-			}
-		}
-		Run_result result=run_calc(input);
-
-		for(auto x:result.result){
-			auto team=x.team;
-			r|=Prediction_status(
-				team,
-				p1[x.team].unplayed_district_events_scheduled,
-				x.dcmp_make,
-				x.cmp_make
-			);
-		}
-
-		auto cmp_make=mapf([](auto x){ return x.cmp_make; },result.result);
-		auto dcmp_make=mapf([](auto x){ return x.dcmp_make; },result.result);
-
-		auto decile_count=[](auto x){
-			std::map<int,size_t> r;
-			for(auto elem:x){
-				r[elem*10]++;
-			}
-			//return r;
-			//cout<<r<<"\n";
-			for(auto i:range(12)){
-				cout<<r[i]<<",";
-			}
-			cout<<"\n";
-		};
-
-		//PRINT(count(cmp_make));
-		//PRINT(count(dcmp_make));
-
-		//decile_count(cmp_make);
-		decile_count(dcmp_make);
-
-		/*for(auto x:result.cutoff_pr){
-			cout<<"cutoff_pr:"<<quartiles(x)<<"\n";
-		}
-		cout<<"cmp_cutoff_pr:"<<quartiles(result.cmp_cutoff_pr)<<"\n";
-		*/
-		//print_r(result);
-	}
-
-
-	//this stuff passes the basic test that looks basically normal.
-	#if 0
-	auto g=group([](auto x){ return x.team; },r);
-	std::vector<pair<Pr,Pr>> dcmp_pr;
-	for(auto [k,v]:g){
-		//PRINT(k);
-		for(auto [a,b]:adjacent_pairs(v)){
-			dcmp_pr|=make_pair(a.dcmp_pr,b.dcmp_pr);
-		}
-	}
-
-	static constexpr int BUCKETS=20;
-	using Bucket=int;
-	auto bucket=[](Pr a)->Bucket{
-		return a*BUCKETS;
-	};
-
-	map<Bucket,std::multiset<Bucket>> m;
-	for(auto [a,b]:dcmp_pr){
-		m[bucket(a)]|=bucket(b);
-	}
-
-	print_r(m);
-	#endif
-
-	//For each week that occurs, how much does the probability change?
-	//broken up by whether or not the team played.
-
-	map<bool,std::vector<double>> changes;
-	for(auto [k,v]:group([](auto x){ return x.team; },r)){
-		for(auto [a,b]:adjacent_pairs(v)){
-			auto d_pr=b.cmp_pr-a.cmp_pr;
-			auto d_events=a.district_events_remaining-b.district_events_remaining;
-			assert(d_events==0 || d_events==1);
-			changes[d_events]|=d_pr;
-		}
-	}
-
-	for(auto [k,v]:changes){
-		PRINT(k);
-		PRINT(quartiles(v));
-		auto abs=MAP(fabs,v);
-		PRINT(mean(abs));
-		PRINT(deciles(v));
-	}
-
-	return r;
-}
-
-int historical_demo(TBA_fetcher &fetcher){
-	/*
-	 *would be interesting to have a function of:
-	 (what week it is,current probability est,whether have event this week) -> 
-	  (distribution of probabilities after this week)
-
-	  known issues: we have teams that are listed as 50/50 (from ties)
-	  even after the event has happened.
-	 * */
-
-	tba::District_key district{"2024pnw"};
-	historical_demo(fetcher,district);
-	historical_demo(fetcher,tba::District_key("2025pnw"));
-	historical_demo(fetcher,tba::District_key("2026pnw"));
-
-	return 0;
-}
-
 struct Args{
 	string output_dir=".";
 	tba::Year year{2022};
 	optional<tba::District_key> district;
 	TBA_fetcher_config tba;
-	bool demo=0,historical_demo=0,rank_limits_demo=0;
+	bool demo=0,rank_limits_demo=0;
 	bool award_limits_demo=0;
 	bool playoff_limits_demo=0;
 	bool timezone_demo=0;
@@ -871,11 +286,6 @@ Args parse_args(int argc,char **argv){
 		"--demo",{},
 		"Explore data for possible future use",
 		r.demo
-	);
-	p.add(
-		"--historical_demo",{},
-		"Explore predictability throughout a season",
-		r.historical_demo
 	);
 	p.add(
 		"--skill",{"METHOD"},
@@ -1063,10 +473,6 @@ int main1(int argc,char **argv){
 
 	if(args.demo){
 		return demo(tba_fetcher);
-	}
-
-	if(args.historical_demo){
-		return historical_demo(tba_fetcher);
 	}
 
 	if(args.lock){
